d4136545c26c57953400b5ac2e3e405fe79a4fb7
Shimanshu83 <shimanshuyadav83@gmail.com>
2023-02-22 14:59:00 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  routes = [{
+    name: 'Home',
+    path: '/home',
+    icon: 'home'
+  }, {
+
+    name: 'Contact',
+    path: '/contact',
+    icon: 'contact'
+
+  }, {
+
+    name: 'About Us',
+    path: '/about_us',
+    icon: 'about_us'
+  }];
+
   title = 'demo-project';
+  panelOpenState = false;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { HomeComponent } from './app/pages/home/home.component';
+import { SidebarComponent } from './app/component/sidebar/sidebar.component';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatExpansionModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
